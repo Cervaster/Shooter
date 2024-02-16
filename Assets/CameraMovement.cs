aa7a53397096a4b2405d7dd785629acb5606086b
Cervaster <77813287+Cervaster@users.noreply.github.com>
2024-02-16 17:25:25 -0500

    Squashed commit of the following:
    commit 2e2241ad12cf832e893f46f3474977b72588541b
    Author: Cervaster <77813287+Cervaster@users.noreply.github.com>
    Date:   Fri Feb 16 15:10:55 2024 -0500
    
        creacion de player

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -16,39 +16,4 @@ public class FPSCameraMovement : MonoBehaviour
     {
         
     }
-
-    private void Update()
-    {
-        h = horizontalSpeed * Input.GetAxis("Mouse X");
-        v = verticalSpeed * Input.GetAxis("Mouse Y");
-
-        transform.Rotate(0, h, 0);
-        fpsCamera.transform.Rotate(-v, 0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0, 0, 0.1f);
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(0, 0, -0.1f);
-            }
-            else
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    transform.Translate(-0.1f, 0, 0);
-                }
-                else
-                {
-                    if (Input.GetKey(KeyCode.D))
-                    {
-                        transform.Translate(0.1f, 0, 0);
-                    }
-                }
-            }
-        }
-    }
 }
